3547ac2011b337965bad45ac4852374f7c9e487b
enlighten96gm <gm.enlightenby@gmail.com>
2022-06-26 23:29:15 +0200

    feat: created task 4

--- my-products-service/getProductsById.js
+++ my-products-service/getProductsById.js
@@ -1,6 +1,22 @@
-import products from "./mocks/products";
+import pg from "pg";
+const { Client } = pg;
+
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+const dbOptions = {
+  host: PG_HOST,
+  port: PG_PORT,
+  database: PG_DATABASE,
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeoutMillis: 5000,
+};
 
 export const getProductsById = async (event, context, callback) => {
+  const client = new Client(dbOptions);
+  await client.connect();
   const headers = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Credentials": true,
@@ -9,13 +25,20 @@ export const getProductsById = async (event, context, callback) => {
   let response;
 
   try {
-    const id = event.pathParameters.productId;
-    // Suppouse to be BD entity here
-    let filteredProduct = await products.filter(
-      (product) => product.id === id
-    )[0];
+    const id = await event.pathParameters.productId;
 
-    if (filteredProduct) {
+    console.log("Event for current product id", id);
+
+    const { rows: filteredProduct } = await client.query(
+      `select id, count, title, description, price 
+      from stocks, products
+      where products.id = stocks.product_id and
+      products.id = ('${id}')`
+    );
+
+    console.log("Filtered result", filteredProduct);
+
+    if (filteredProduct.length === 1) {
       response = {
         statusCode: 200,
         headers: headers,
@@ -32,12 +55,26 @@ export const getProductsById = async (event, context, callback) => {
     }
     callback(null, response);
   } catch (error) {
-    response = {
-      statusCode: 500,
-      headers: headers,
-      isBase64Encoded: false,
-      body: JSON.stringify({ message: error.message }),
-    };
-    callback(error, response);
+    console.error(error, "Catch Error");
+
+    if (error.code === "22P02") {
+      response = {
+        statusCode: 404,
+        headers: headers,
+        isBase64Encoded: false,
+        body: JSON.stringify({ message: "Syntax error in id" }),
+      };
+    } else {
+      response = {
+        statusCode: 500,
+        headers: headers,
+        isBase64Encoded: false,
+        body: JSON.stringify({ message: error.message }),
+      };
+    }
+
+    callback(null, response);
+  } finally {
+    client.end();
   }
 };
--- my-products-service/getProductsList.js
+++ my-products-service/getProductsList.js
@@ -1,17 +1,56 @@
-import products from "./mocks/products";
+import pg from "pg";
+const { Client } = pg;
+
+const { PG_HOST, PG_PORT, PG_DATABASE, PG_USERNAME, PG_PASSWORD } = process.env;
+const dbOptions = {
+  host: PG_HOST,
+  port: PG_PORT,
+  database: PG_DATABASE,
+  user: PG_USERNAME,
+  password: PG_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  connectionTimeoutMillis: 5000,
+};
 
 export const getProductsList = async (event, context, callback) => {
-  // Suppouse to be BD entity here
-  const wholeProductsEntity = await products;
-  const response = {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Credentials": true,
-      "Content-Type": "application/json;charset=UTF-8",
-    },
-    body: JSON.stringify(wholeProductsEntity),
-    isBase64Encoded: false,
+  const client = new Client(dbOptions);
+  await client.connect();
+  let response;
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Credentials": true,
+    "Content-Type": "application/json;charset=UTF-8",
   };
-  callback(null, response);
+
+  try {
+    const { rows: products } = await client.query(`select * from products`);
+
+    console.log(products);
+
+    response = {
+      statusCode: 200,
+      headers: headers,
+      body: JSON.stringify(products),
+      isBase64Encoded: false,
+    };
+
+    callback(null, response);
+  } catch (err) {
+    console.error(err, "Catch Error");
+
+    response = {
+      statusCode: 500,
+      headers: headers,
+      body: JSON.stringify({
+        message: "Unhandled error =(",
+      }),
+      isBase64Encoded: false,
+    };
+
+    callback(response, null);
+  } finally {
+    client.end();
+  }
 };
